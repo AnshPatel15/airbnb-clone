1a72baaec143e08c9d60ac75a4d0d2f58803e046
AnshPatel15 <anjalasnsh@gmail.com>
2023-05-19 17:48:32 +0530

    button-comp-finish

---
+++ app/components/ClientOnly.tsx
@@ -0,0 +1,22 @@
+"use client";
+import { useEffect, useState } from "react";
+
+interface CLientOnlyProps {
+  children: React.ReactNode;
+}
+
+const ClientOnly: React.FC<CLientOnlyProps> = ({ children }) => {
+  const [hasMounted, setHasMounted] = useState(false);
+
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
+
+  if (!hasMounted) {
+    return null;
+  }
+
+  return <div>{children}</div>;
+};
+
+export default ClientOnly;
