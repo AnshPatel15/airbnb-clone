783345e74f38ddb3db23eaab632525996b109bf6
AnshPatel15 <anjalasnsh@gmail.com>
2023-05-24 16:52:16 +0530

    added routes to categories

--- app/components/Navbar/Categories.tsx
+++ app/components/Navbar/Categories.tsx
@@ -5,6 +5,7 @@ import { TbBeach } from "react-icons/tb";
 import { GiWindmill } from "react-icons/gi";
 import { MdOutlineVilla } from "react-icons/md";
 import CategoryBox from "../CategoryBox";
+import { useSearchParams } from "next/navigation";
 
 export const categories = [
   {
@@ -25,17 +26,19 @@ export const categories = [
 ];
 
 const Categories = () => {
+  const params = useSearchParams();
+
   return (
     <Container>
-          <div className="pt-4 flex flex-row items-center justify-between overflow-x-auto">
-              {categories.map(item) => {
-                  <CategoryBox
-                      key={item.label}
-                      label={item.label}
-                      description={item.description}
-                      icon={item.icon}
-                  />
-              }}
+      <div className="pt-4 flex flex-row items-center justify-between overflow-x-auto">
+        {categories.map((item) => (
+          <CategoryBox
+            key={item.label}
+            label={item.label}
+            description={item.description}
+            icon={item.icon}
+          />
+        ))}
       </div>
     </Container>
   );
